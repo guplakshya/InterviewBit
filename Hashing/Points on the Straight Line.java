e1750c17573f4a7ae125712e47bb628a96908b49
Lakshya Porwal <42773640+guplakshya@users.noreply.github.com>
2021-02-07 09:59:34 +0530

    Create Points on the Straight Line.java

---
+++ Hashing/Points on the Straight Line.java
@@ -0,0 +1,60 @@
+############### Points on the Straight Line ###############
+
+/*
+
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+
+*/
+
+Java Solution:
+
+public int maxPoints(ArrayList<Integer> a, ArrayList<Integer> b) {
+        int maxPoints = 0;
+        HashMap<Double, Integer> map = new HashMap<Double, Integer>();
+        if(a.size() != b.size() || a.size() == 0 || a == null || b.size() == 0 || b == null)
+            return 0;
+        if(a.size() == 1 && b.size() == 1)
+            return 1;
+        for(int i = 0; i < a.size(); i++){
+            int duplicate = 1;
+            int vertical = 0;
+            int xi = a.get(i);
+            int yi = b.get(i);
+            for(int j = i+1; j < a.size(); j++){
+                int xj = a.get(j);
+                int yj = b.get(j);
+                if(xi == xj){
+                    if(yi == yj){
+                        duplicate++;
+                    }else{
+                        vertical++;
+                    }
+                }
+                else{
+                    double slope = 0.0;
+                    if(yj == yi)
+                        slope = 0.0;
+                    else if(xj == xi )
+                        slope = Double.MAX_VALUE;
+                    else
+                        slope = (double)(yj - yi) / (double)(xj - xi);
+                    
+                    if(map.containsKey(slope))
+                        map.put(slope, map.get(slope) + 1);
+                    else 
+                        map.put(slope, 1);
+                }
+            }
+            for(int slp : map.values())
+                if(maxPoints < slp + duplicate)
+                maxPoints = slp + duplicate;
+            
+            maxPoints = Math.max(vertical + duplicate, maxPoints);
+            map.clear();
+        }
+        
+        
+        return maxPoints;
+    }
+    
+    
